6d26db4952bf7558f5213f7dccb9d6df19f51732
Mary Whistler <marywhistler@me.com>
2020-10-03 15:24:43 -0400

    add

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,3 +1,60 @@
 // @TODO: YOUR CODE HERE!
-var svgHeight = 600;
-var svgWidth = 400;
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 500;
+
+// Define the chart's margins as an object
+var margin = {
+  top: 60,
+  right: 60,
+  bottom: 60,
+  left: 60
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+
+
+d3.csv("data.csv").then(function(JData) {
+
+    // Print the Data
+    console.log(JData);
+  
+var svg = d3.select("#svg-area").append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+  var trace1 = {
+    x: [1, 2, 3, 4, 5],
+    y: [1, 6, 3, 6, 1],
+    mode: 'markers',
+    type: 'scatter',
+    name: 'Team A',
+    text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
+    marker: { size: 12 }
+  };
+  
+  var trace2 = {
+    x: [1.5, 2.5, 3.5, 4.5, 5.5],
+    y: [4, 1, 7, 1, 4],
+    mode: 'markers',
+    type: 'scatter',
+    name: 'Team B',
+    text: ['B-a', 'B-b', 'B-c', 'B-d', 'B-e'],
+    marker: { size: 12 }
+  };
+  
+  var data = [ trace1, trace2 ];
+  
+  var layout = {
+    xaxis: {
+      range: [ 0.75, 5.25 ]
+    },
+    yaxis: {
+      range: [0, 8]
+    },
+    title:'Data Labels Hover'
+  };
+  
+  Plotly.newPlot('myDiv', data, layout);
