73af8dd0c19ac1d7ca21309aaa24ab499f360e7c
Mary Whistler <marywhistler@me.com>
2020-10-03 23:48:18 -0400

    add

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -16,45 +16,18 @@ var chartWidth = svgWidth - margin.left - margin.right;
 var chartHeight = svgHeight - margin.top - margin.bottom;
 
 
-d3.csv("data.csv").then(function(JData) {
+d3.csv("data.csv").then(function(data) {
 
     // Print the Data
-    console.log(JData);
+    console.log(data);
   
-var svg = d3.select("#svg-area").append("svg")
+var svg = d3.select(".chart")
+    .append("svg")
   .attr("height", svgHeight)
   .attr("width", svgWidth);
+  
 
-  var trace1 = {
-    x: [1, 2, 3, 4, 5],
-    y: [1, 6, 3, 6, 1],
-    mode: 'markers',
-    type: 'scatter',
-    name: 'Team A',
-    text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
-    marker: { size: 12 }
-  };
-  
-  var trace2 = {
-    x: [1.5, 2.5, 3.5, 4.5, 5.5],
-    y: [4, 1, 7, 1, 4],
-    mode: 'markers',
-    type: 'scatter',
-    name: 'Team B',
-    text: ['B-a', 'B-b', 'B-c', 'B-d', 'B-e'],
-    marker: { size: 12 }
-  };
-  
-  var data = [ trace1, trace2 ];
-  
-  var layout = {
-    xaxis: {
-      range: [ 0.75, 5.25 ]
-    },
-    yaxis: {
-      range: [0, 8]
-    },
-    title:'Data Labels Hover'
-  };
-  
-  Plotly.newPlot('myDiv', data, layout);
+  Data.forEach(function(data) {
+    data.age = +data.age;
+    data.smokes = +data.smokes;
+  });
